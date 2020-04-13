f01cad327521684e79ea40cbdacad219cf82dcc8
anna bateman <annacbateman@gmail.com>
2020-04-13 22:46:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,10 @@
 def reverse_each_word (string)
   
-  reverse = string.split("").map do |element|
+  new_string = string.split("").map do |element|
   element.reverse
-
+  
   # reverse_each_word.join
   
   end
-  reverse.join
   # reverse_each_word.to_s
 end
