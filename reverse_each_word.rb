a595230a21c19455d0a39036b5b5dae416fa5fe6
anna bateman <annacbateman@gmail.com>
2020-04-13 22:51:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,3 +9,11 @@ def reverse_each_word (string)
   new_string.join
   # reverse_each_word.to_s
 end
+
+
+# dwarf_hash = Hash.new
+#   array.each_with_index do |dwarf, index|
+#     dwarf_hash[dwarf] = index
+#     puts "#{index + 1}. #{dwarf.capitalize}"
+#   end
+#   dwarf_hash
