4c53a349fee41620e974f7ddb7c7fd32d413d7b4
anna bateman <annacbateman@gmail.com>
2020-04-13 22:58:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,6 @@
 def reverse_each_word (string)
   new_string = string.split(" ").collect do |element|
   element.reverse
-  # new_string.join
-  # reverse_each_word.join
-  
   end
   new_string.join(" ")
-  # reverse_each_word.to_s
 end
-
-
-# dwarf_hash = Hash.new
-#   array.each_with_index do |dwarf, index|
-#     dwarf_hash[dwarf] = index
-#     puts "#{index + 1}. #{dwarf.capitalize}"
-#   end
-#   dwarf_hash
