90d10431212e240236fa3b29d337c0a50e0b63bd
anna bateman <annacbateman@gmail.com>
2020-04-13 23:13:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word (string)
-  new_string = string.split(" ").collect do |element|
+  new_string = string.split("").collect do |element|
   element.reverse
   end
   new_string.join(" ")
